2fc656f4e4aacbdf377be5d894ff2c6c4dc797c5
SOPHIA FANTINI DOS SANTOS SOUZA <55495571851@imaccommand027.local>
2025-11-18 10:46:57 -0300

    exerciciojava.js

---
+++ ecerciciojava.js
@@ -0,0 +1,33 @@
+ let login = window.prompt("Qual seu nome?")
+ let senha = window.prompt("Qual sua senha?")
+
+ switch (login) {
+    case "admin":
+        console.log("Usuário Correto. bem-vindo")
+        break;
+ 
+    default:
+        console.log("Usuário incorreto. tente novamente")
+        break;
+ }
+ switch (Senha) {
+    case "Senha123":
+        console.log("Senha Correta. bem sucedido!")
+        break;
+ 
+    default:
+        console.log("Senha incorreta. Tente novamente!")
+        break;
+ }
+
+ let login = prompt("digite seu usuário: ")
+
+ let login = prompt("Digite sua senha: ")
+
+ if (login == "admin" && senha == "Senha123") {
+    console.log("login bem sucedido!")
+ }
+
+ else{
+    console.log("Voce nao ter permissao a esse acesso!")
+ }
