735fa041b23d0346a2c483b6adbd256a1283ec60
SOPHIA FANTINI DOS SANTOS SOUZA <55495571851@imaccommand027.local>
2025-11-18 15:44:23 -0300

    exercicio04

--- exercicio03.js
+++ exercicio03.js
@@ -1,9 +1,9 @@
 
-let numero = parseInt( prompt("Informe um numero inteiro:"))
+let numero = parseInt(window.prompt("Informe um numero inteiro:"))
 
-if(numero%2 == 0){
-   console.log
-
-
+if(numero % 2 == 0) {
+   console.log("O numero é par")
 }
-
+    else{
+        console.log("O numero é impar ")
+    }
